9e3ff5c42235b2a25ecc49edace9ad918539894e
Liz Daly <liz7daly@gmail.com>
2020-09-09 21:51:33 +0100

    Update full screen element

--- src/fullScreen.js
+++ src/fullScreen.js
@@ -1,11 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
   var element = document.getElementById('item-full-screen')
+  element.addEventListener('click', toggleFullScreen)
 
-  element.addEventListener('click', function() {
-    if(element.requestFullscreen) {
+  function toggleFullScreen() {
+    if(!document.fullscreenElement) {
       element.requestFullscreen();
       element.classList.add('full-screen')
       document.getElementById('full-screen-toggle').textContent = 'Exit full screen'
+    } else {
+      if(document.exitFullscreen) {
+        document.exitFullscreen()
+        element.classList.remove('full-screen')
+      document.getElementById('full-screen-toggle').textContent = 'Make me full screen'
+      }
     }
-  })
-})
+  }
+ })
