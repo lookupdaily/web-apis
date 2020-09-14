c454c189ff73c015b949b2582226f20e12b24c4e
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2020-09-14 10:38:48 +0100

    Add reset background button

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('toggle-bg').addEventListener('click', toggleBackground)
+  document.getElementById('reset-bg').addEventListener('click', resetBackground)
 })
 
 function toggleBackground() {
-  colours = ['AliceBlue', 'FloralWhite', 'GhostWhite', 'HoneyDew', 'LavenderBlush', 'Linen', 'MintCream', 'MistyRose', 'OldLace', 'WhiteSmoke'];
+  colours = ['AliceBlue', 'FloralWhite', 'HoneyDew', 'LavenderBlush', 'Linen', 'MintCream', 'MistyRose', 'OldLace', 'WhiteSmoke'];
   var colour = colours[Math.floor(Math.random() * colours.length)];
 
   document.querySelector('body').style.backgroundColor = colour;
+}
+
+function resetBackground() {
+  document.querySelector('body').style.backgroundColor = 'GhostWhite';
 }
 
